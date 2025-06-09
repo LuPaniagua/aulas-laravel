22a5c42695cdc5fc46e748279d6bffb16769fe31
LUAN VICTOR PANIAGUA CASTRO <luan.vpcastro@senacsp.edu.br>
2025-06-09 11:42:27 -0300

    aula 1

--- routes/web.php
+++ routes/web.php
@@ -9,25 +9,3 @@ Route::get('/', function () {
 Route::get('/sobrenos', function(){
     return 'Essa é a página sobre nós';
 });
-
-Route::get('/login', function(){
-    return 'Essa é a página de login';
-});
-
-Route::get('/salas', function(){
-    return 'Essa é a página com salas com rodas de conversa';
-});
-
-Route::get('/perfil', function(){
-    return 'Essa é a página que irá mostrar o seu perfil com seus laudos';
-});
-
-Route::get('/rodas', function(){
-    return 'Essa é dentro das salas de rodas de conversa';
-});
-
-Route ::get('/contatos', function () {
-     return 'Essa é a pagína sobre os contatos'; });
-
-Route ::get('/contrato', function () { 
-    return 'Essa é a pagína para contratar um profissional'; });
